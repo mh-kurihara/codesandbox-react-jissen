5aa683c4eec054ef6ac19aafb31852ef6a2e863e
mh-kurihara <108643305+mh-kurihara@users.noreply.github.com>
2022-08-17 18:28:19 +0900

    Initial commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,14 @@
+import { memo, VFC } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Login } from "../components/pages/Login";
+
+export const Router: VFC = memo(() => {
+  return (
+    <Routes>
+      <Route>
+        <Login />
+      </Route>
+    </Routes>
+  );
+});
